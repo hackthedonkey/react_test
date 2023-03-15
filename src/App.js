36d0a4fba33d24a4d13d8fc39d34295f77b1a228
luke <gwmyeong@megazone.com>
2023-03-15 10:51:58 +0900

    add build.ymml

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Counter</h1>
       </header>
+      <div className="counter-container">
+        <div className="count">{count}</div>
+        <div className="button-container">
+          <button onClick={incrementCount}>+</button>
+          <button onClick={decrementCount}>-</button>
+        </div>
+      </div>
     </div>
   );
 }
